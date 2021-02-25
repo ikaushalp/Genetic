5f8ae0d5f96d53b4b51585c18746c6bb0e11a770
karankhojiji <khojijikaran@gmail.com>
2021-02-25 12:43:26 +0530

    Ajax Call Under Development

--- Patient/views.py
+++ Patient/views.py
@@ -1,33 +1,33 @@
 from django.shortcuts import render,HttpResponse
 from Patient.models import Patient
 from django.http import JsonResponse
-from django.views.generic import TemplateView
 
 # Patient #
-def add_Patient(request):
+def add_patient(request):
     if request.method == 'POST':
-        add = Patient(
-            name=request.POST['name'],
-            gender=request.POST['gender'],
-            birthdate=request.POST['birthdate'],
-            age=request.POST['age'],
-            marital_status=request.POST['marital_status'],
-            mobile_no=request.POST['phone'],
-            email=request.POST['email'],
-            category=request.POST['category'],
-            blood_group=request.POST['blood_group'],
-            blood_pressure=request.POST['blood_pressure'],
-            height=request.POST['height'],
-            weight=request.POST['weight'],
-            address=request.POST['address'],
-            guardian_name=request.POST['guardian_name'],
-            relationship=request.POST['relationship'],
-            guardian_mobile_no=request.POST['guardian_mobile'],
-        )
-        add.save()
-        return JsonResponse('')
-    else:
-        return render(request, 'Patient_template/add_patient.html')
+        # add = Patient(
+        #     name=request.POST['name'],
+        #     gender=request.POST['gender'],
+        #     birthdate=request.POST['birthdate'],
+        #     age=request.POST['age'],
+        #     marital_status=request.POST['marital_status'],
+        #     mobile_no=request.POST['phone'],
+        #     email=request.POST['email'],
+        #     category=request.POST['category'],
+        #     blood_group=request.POST['blood_group'],
+        #     blood_pressure=request.POST['blood_pressure'],
+        #     height=request.POST['height'],
+        #     weight=request.POST['weight'],
+        #     address=request.POST['address'],
+        #     guardian_name=request.POST['guardian_name'],
+        #     relationship=request.POST['relationship'],
+        #     guardian_mobile_no=request.POST['guardian_mobile'],
+        # )
+        # add.save()
+        # return JsonResponse('')
+        data = {'success': 1}
+        return JsonResponse(data)
+    return render(request, 'Patient_template/add_patient.html')
 
 
 
