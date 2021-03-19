40dce4dec0f444176688515dc11714c8bbe36017
karankhojiji <khojijikaran@gmail.com>
2021-03-19 22:56:40 +0530

    All Working Bruh !!

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from Patient.models import Patient
+from Authentication.models import CustomUser
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'Dashboard_template/dashboard.html')
+    patient = Patient.objects.count()
+    user = CustomUser.objects.count()
+    context = {'patient': patient, 'user': user}
+    return render(request, 'Dashboard_template/dashboard.html', context=context)
