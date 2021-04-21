3aab967e0cbe23f5345352607cc1a58f5011ffe8
karankhojiji <khojijikaran@gmail.com>
2021-04-21 14:04:09 +0530

    New Login Page Validation Update

--- static/js/pages/custom/login/login-general.js
+++ static/js/pages/custom/login/login-general.js
@@ -1,7 +1,7 @@
-let KTLogin = function() {
-    let login;
+jQuery(document).ready(function () {
+    let login = $('#kt_login');
 
-    let showForm = function(form) {
+    let showForm = function (form) {
         let cls = 'login-' + form + '-on';
         let Form = 'kt_login_' + form + '_form';
 
@@ -9,129 +9,123 @@ let KTLogin = function() {
         login.removeClass('login-signin-on');
 
         login.addClass(cls);
-
+        if (Form === 'kt_login_forgot_form'){
+            forgot_password_validation.resetForm();
+        }
+        if (Form === 'kt_login_signin_form'){
+            signin_validation.resetForm();
+        }
         KTUtil.animateClass(KTUtil.getById(Form), 'animate__animated animate__backInUp');
     }
 
-    let handleSignInForm = function() {
-        let validation;
+// Start::Login Page //
+    let signin_validation;
 
-        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
-        validation = FormValidation.formValidation(
-			KTUtil.getById('kt_login_signin_form'),
-			{
-				fields: {
-					username: {
-						validators: {
-							notEmpty: {
-								message: 'Username is required'
-							}
-						}
-					},
-					password: {
-						validators: {
-							notEmpty: {
-								message: 'Password is required'
-							}
-						}
-					}
-				},
-				plugins: {
-                    trigger: new FormValidation.plugins.Trigger(),
-					bootstrap: new FormValidation.plugins.Bootstrap()
-				}
-			}
-		);
-
-        $('#kt_login_signin_submit').on('click', function (e) {
-            e.preventDefault();
-
-            validation.validate().then(function(status) {
-		        if (status === 'Valid') {
-					console.log("Valid");
-				} else {
-					console.log("Invalid");
-				}
-		    });
-        });
-
-        // Handle forgot button
-        $('#kt_login_forgot').on('click', function (e) {
-            e.preventDefault();
-            showForm('forgot');
-        });
-    }
-
-    let handleForgotForm = function() {
-        let validation;
-
-        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
-        validation = FormValidation.formValidation(
-			KTUtil.getById('kt_login_forgot_form'),
-			{
-				fields: {
-					email: {
-						validators: {
-							notEmpty: {
-								message: 'Email address is required'
-							},
-                            emailAddress: {
-								message: 'The value is not a valid email address'
-							}
-						}
-					}
-				},
-				plugins: {
-					trigger: new FormValidation.plugins.Trigger(),
-					bootstrap: new FormValidation.plugins.Bootstrap()
-				}
-			}
-		);
-
-        // Handle submit button
-        $('#kt_login_forgot_submit').on('click', function (e) {
-            e.preventDefault();
-
-            validation.validate().then(function(status) {
-		        if (status === 'Valid') {
-                    // Submit form
-                    KTUtil.scrollTop();
-				} else {
-					swal.fire({
-		                text: "Sorry, looks like there are some errors detected, please try again.",
-		                icon: "error",
-		                buttonsStyling: false,
-		                confirmButtonText: "Ok, got it!",
-                        customClass: {
-    						confirmButton: "btn font-weight-bold btn-light-primary"
-    					}
-		            }).then(function() {
-						KTUtil.scrollTop();
-					});
-				}
-		    });
-        });
-
-        // Handle cancel button
-        $('#kt_login_forgot_cancel').on('click', function (e) {
-            e.preventDefault();
-            showForm('signin');
-        });
-    }
-
-    // Public Functions
-    return {
-        // public functions
-        init: function() {
-            login = $('#kt_login');
-
-            handleSignInForm();
-            handleForgotForm();
+    signin_validation = FormValidation.formValidation(
+        KTUtil.getById('kt_login_signin_form'),
+        {
+            fields: {
+                username: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Username is required'
+                        }
+                    }
+                },
+                password: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Password is required'
+                        }
+                    }
+                }
+            },
+            plugins: {
+                trigger: new FormValidation.plugins.Trigger(),
+                bootstrap: new FormValidation.plugins.Bootstrap()
+            }
         }
-    };
-}();
+    );
 
-// Class Initialization
-jQuery(document).ready(function() {
-    KTLogin.init();
+    $('#kt_login_signin_submit').on('click', function (e) {
+        e.preventDefault();
+
+        signin_validation.validate().then(function (status) {
+            if (status === 'Valid') {
+                $.ajax({
+                    type: 'POST',
+                    url: 'login',
+                    data: $('#kt_login_signin_form').serialize(),
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.success === 1) {
+                            window.location = '/dashboard';
+                        } else if (data.NotExist === 1) {
+                            Swal.fire(
+                                "Error",
+                                "Invalid Credentials",
+                                "error"
+                            )
+                        }
+                    },
+                });
+            } else {
+                console.log("Invalid");
+            }
+        });
+    });
+
+    // Handle forgot button
+    $('#kt_login_forgot').on('click', function (e) {
+        e.preventDefault();
+        showForm('forgot');
+    });
+
+// End::Login Page //
+
+// Start::Forgot Password Page //
+    let forgot_password_validation;
+
+    forgot_password_validation = FormValidation.formValidation(
+        KTUtil.getById('kt_login_forgot_form'),
+        {
+            fields: {
+                email: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Email address is required'
+                        },
+                        emailAddress: {
+                            message: 'The value is not a valid email address'
+                        }
+                    }
+                }
+            },
+            plugins: {
+                trigger: new FormValidation.plugins.Trigger(),
+                bootstrap: new FormValidation.plugins.Bootstrap()
+            }
+        }
+    );
+
+    // Handle submit button
+    $('#kt_login_forgot_submit').on('click', function (e) {
+        e.preventDefault();
+
+        forgot_password_validation.validate().then(function (status) {
+            if (status === 'Valid') {
+                KTUtil.scrollTop();
+            } else {
+                KTUtil.scrollTop();
+            }
+        });
+    });
+
+    // Handle cancel button
+    $('#kt_login_forgot_cancel').on('click', function (e) {
+        e.preventDefault();
+        showForm('signin');
+    });
+
+// End::Forgot Password Page //
 });
