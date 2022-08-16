9385bf916385162da36b1c1b3da110a27455a780
Kaushal Patel <55987371+mrkaushal@users.noreply.github.com>
2022-08-16 09:36:32 +0000

    Changes

--- Genetic/settings.py
+++ Genetic/settings.py
@@ -129,10 +129,10 @@ USE_TZ = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_LOCALTIME = True
 EMAIL_USE_SSL = True
-EMAIL_HOST = 'mail.getlocalhost.com'
+EMAIL_HOST = ''
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'genetic@getlocalhost.com'
-EMAIL_HOST_PASSWORD = '#Genetic8741#'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
